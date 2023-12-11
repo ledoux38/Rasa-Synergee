654258edb128728fba6e1a590e2a00f62926cf50
Florian LEDOUX <ledoux.florian30@gmail.com>
2023-12-11 14:18:13 +0100

    first

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Rasa-Synergee
 voir les possibilités de rasa
+
+# Installation
+1- Installer pyenv
+
+2- Installer python version 3.9 ```pyenv install 3.8.0```
+
+3- Dans le dossier Rasa-Synergee taper ligne de commande ```pyenv local 3.9```
+
+4- Si virtualenv n'est pas déjà installé, vous pouvez l'installer avec pip : ```pip install virtualenv```
+
+5- Créer un environnement virtuel ```virtualenv venv```
+
+6- Activer l'environnement virtuel ```source venv/bin/activate```
+
+7- Installer Rasa ```pip install rasa```
+
+8- initialiser Rasa ```rasa init```
+
+
+# Ligne de commande utiles:
+1- Rasa:
+
+    - apprendre: ```rasa train```
+
---
+++ actions/actions.py
@@ -0,0 +1,53 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionCreateStore(Action):
+
+    def name(self) -> Text:
+        return "action_create_store"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        store_name = tracker.get_slot('store_name')
+        store_location = tracker.get_slot('store_location')
+
+        # Ici, ajoutez la logique pour créer un magasin dans votre système.
+        # Par exemple, en faisant une requête à votre API ou base de données.
+
+        dispatcher.utter_message(text=f"Le magasin '{store_name}' a été créé à '{store_location}'.")
+
+        return []
