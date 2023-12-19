a92393c266fab017351fbac631c00874c9aa4bc5
Florian LEDOUX <ledoux.florian30@gmail.com>
2023-12-19 13:17:52 +0100

    change interface

--- interface/script.js
+++ interface/script.js
@@ -1,13 +1,27 @@
 document.getElementById("send-button").addEventListener("click", function() {
     var userInput = document.getElementById("user-input").value;
+    document.getElementById("user-input").value = ""; // Clear input field
     sendMessage(userInput);
 });
 
-function sendMessage(message) {
+function displayUserMessage(message) {
     var chatBox = document.getElementById("chat-box");
-    var userMessageDiv = document.createElement("div");
-    userMessageDiv.textContent = "Vous: " + message;
-    chatBox.appendChild(userMessageDiv);
+    var messageDiv = document.createElement("div");
+    messageDiv.textContent = "Vous: " + message;
+    messageDiv.classList.add("message", "user-message");
+    chatBox.appendChild(messageDiv);
+}
+
+function displayBotMessage(message) {
+    var chatBox = document.getElementById("chat-box");
+    var messageDiv = document.createElement("div");
+    messageDiv.textContent = "Bot: " + message;
+    messageDiv.classList.add("message", "bot-message");
+    chatBox.appendChild(messageDiv);
+}
+
+function sendMessage(message) {
+    displayUserMessage(message);
 
     fetch('http://localhost:5005/webhooks/rest/webhook', {
         method: 'POST',
@@ -19,9 +33,7 @@ function sendMessage(message) {
     .then(response => response.json())
     .then(data => {
         data.forEach(messageData => {
-            var botMessageDiv = document.createElement("div");
-            botMessageDiv.textContent = "Bot: " + messageData.text;
-            chatBox.appendChild(botMessageDiv);
+            displayBotMessage(messageData.text);
         });
     })
     .catch(error => console.error('Error:', error));
